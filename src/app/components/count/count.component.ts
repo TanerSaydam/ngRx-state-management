b8ece49eb24d57ea0cf6af912e37c873927fb83d
taner <taner@DESKTOP-3BJ5GK9>
2022-11-04 15:42:49 +0300

    .

--- src/app/components/count/count.component.ts
+++ src/app/components/count/count.component.ts
@@ -1,6 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { count, Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { CountService } from 'src/app/services/count.service';
 
 @Component({
